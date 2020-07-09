ea70de72cd167ba50f09b72042fc8221742b9783
Mon0106 <dongnam.japan@gmail.com>
2020-07-09 10:32:30 +0700

    mypak

---
+++ src/AccessModifier/mypack/B.java
@@ -0,0 +1,10 @@
+package AccessModifier.mypack;
+
+import AccessModifier.pack.*;
+public class B {
+    public static void main(String[] args) {
+        A obj = new A();
+
+        obj.msg();
+    }
+}
